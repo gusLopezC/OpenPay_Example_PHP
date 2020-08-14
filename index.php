c568b48435458b7cad02279d6438978df094f6e5
gusLopezC <guslopezcallejas@gmail.com>
2020-08-14 15:48:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,110 +5,67 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style2.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script type="text/javascript" src="https://openpay.s3.amazonaws.com/openpay.v1.min.js"></script>
     <script type='text/javascript' src="https://openpay.s3.amazonaws.com/openpay-data.v1.min.js"></script>
 </head>
-
 <body>
-    
 </body>
 <div class="container">
     <div class="row">
-        <div class="col-sm-9">
-            <form action="#" id="payment-form">
-                <input type="hidden" name="token_id" id="token_id">
-                <input type="hidden" name="use_card_points" id="use_card_points" value="false">
-                <div class="pymnt-itm card active">
-                    <h2>Tarjeta de crédito o débito</h2>
-                    <div class="pymnt-cntnt">
-                        <div class="card-expl">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <div class="credit">
-                                        <h4>Tarjetas de crédito</h4>
-                                    </div>
-                                </div>
-                                <div class="col-sm-5">
-                                    <div class="debit">
-                                        <h4>Tarjetas de débito</h4>
-                                    </div>
-                                </div>
-                            </div>
-    
-    
-                        </div>
-                        <br>
-                        <div class="sctn-row row">
-                            <div class="sctn-col l">
-                                <label>Nombre del titular</label>
-                                <input type="text" placeholder="Como aparece en la tarjeta"
-                                    autocomplete="off" name="name" required data-openpay-card="holder_name">
-                            </div>
-                            <div class="sctn-col">
-                                <label>Número de tarjeta</label>
-                                <input type="text" autocomplete="off" required
-                                    data-openpay-card="card_number"></div>
-                        </div>
-                        <div class="sctn-row row">
-                            <div class="sctn-col l">
-                                <label>Fecha de expiración</label>
-                                <div class="sctn-col half l"><input type="text" placeholder="Mes" required
-                                        data-openpay-card="expiration_month"></div>
-                                <div class="sctn-col half l"><input type="text" placeholder="Año" required
-                                        data-openpay-card="expiration_year"></div>
-                            </div>
-                            <div class="sctn-col cvv"><label>Código de seguridad</label>
-                                <div class="sctn-col half l"><input type="text" placeholder="3 dígitos" required autocomplete="off"
-                                        data-openpay-card="cvv2"></div>
-                            </div>
-                        </div>
-                        <div class="openpay row">
-                            <div class="logo col-sm-4">Transacciones realizadas vía:</div>
-                            <div class="shield">Tus pagos se realizan de forma segura con encriptación de 256 bits</div>
-                        </div>
-                        <br>
-                        <br>
-                        <br><br>
-    
-                        <div class="sctn-row">
-                            <button type="submit" class="btn btn-primary btn-lg btn-block" id="pay-button">Pagar</button>
-                            
-                        </div>
+        <div class="col-sm-8">
+            <div class="form-container">
+                <div class="personal-information">
+                    <h1>Payment Information</h1>
+                </div> <!-- end of personal-information -->
+                <form action="#" id="payment-form">
+                    <input type="hidden" name="token_id" id="token_id">
+                    <input type="hidden" name="use_card_points" id="use_card_points" value="false">
+                    <input id="input-field" type="text" placeholder="Name Complet" autocomplete="off" name="name" required data-openpay-card="holder_name" />
+                    <input id="input-field" type="text" data-openpay-card="card_number" placeholder="Card Number" value="4242424242424242" />
+                    <input id="column-left" style="width: 24%; margin-right: 5px;" type="text" name="expiry month" placeholder="MM" data-openpay-card="expiration_month" value="02" />
+                    <input id="column-left" style="width: 24%;" type="text" name="expiry year" placeholder="YY" data-openpay-card="expiration_year" value="21" />
+                    <input id="column-right" type="text" name="cvc" placeholder="CCV" data-openpay-card="cvv2" value="102" />
+                    <div class="card-wrapper"></div>
+                    <input id="input-field" type="text" name="streetaddress" required="required" autocomplete="on" maxlength="45" placeholder="Streed Address" />
+                    <input id="column-left" type="text" name="city" required="required" autocomplete="on" maxlength="20" placeholder="City" />
+                    <input id="column-right" type="text" name="zipcode" required="required" autocomplete="on" pattern="[0-9]*" maxlength="5" placeholder="ZIP code" />
+                    <input id="input-field" type="email" name="email" required="required" autocomplete="on" maxlength="40" placeholder="Email" />
+                    <input type="submit" id="pay-button" value="Submit" />
+                </form>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="card" style="width:400px">
+                <img class="card-img-top" src="https://ss628.liverpool.com.mx/xl/1097268092.jpg" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-title">Laptop Dell Inspiron <strong>$11,969</strong></h4>
+                    <p class="card-text"> Modelo 3000 de 15.6.</p>
+                    <a href="#" class="btn btn-primary">Ver más ...</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="row">
+        <div class="col-sm-12">
+            <div id="myDIV" style="border:1px solid black; display:none;">
+                <div class="card" style="width:100%">
+                    <div class="card-body">
+                        <h5 class="card-title">Pago exitoso</h5>
+                        <div id="content"></div>
                     </div>
                 </div>
-            </form>
-        </div>
-        <div class="col-sm-2">
-        <div style="margin-bottom: 30%;"></div>
-        <div class="card" style="width:400px">
-  <img class="card-img-top" src="img_avatar1.png" alt="Card image">
-  <div class="card-body">
-    <h4 class="card-title">John Doe</h4>
-    <p class="card-text">Some example text.</p>
-    <a href="#" class="btn btn-primary">See Profile</a>
-  </div>
-</div>
+            </div>
         </div>
     </div>
 </div>
-
-<script
-  src="https://code.jquery.com/jquery-3.5.1.js"
-  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-  crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
-    crossorigin="anonymous"></script>
-
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <script type="text/javascript" src="assets/js/pay.js"></script>
-
 </html>
